3ea555dfd9bdd414024d05cccb62c249688f47ca
AjayM2004 <ajay97908940@gmail.com>
2025-05-21 22:25:00 +0530

    prepared for the deployment

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -4,17 +4,21 @@ import NotesRoutes from './Routes/NotesRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from "dotenv";
 import cors from 'cors';
+import path from 'path';
 import rateLimiter from './Middleware/RateLimiter.js';
 // Load environment variables from .env file
 dotenv.config();
-
 const app = express();
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 5001; // Set the port to the value in .env or default to 5001
 // CORS middleware (Cross-Origin Resource Sharing. Allows requests from different origins)
 // This is important for allowing your frontend to communicate with your backend
-app.use(cors({
-  origin: "http://localhost:5173",
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({
+    origin: "http://localhost:5173",
+  }));
+}
 
-}));
 // Middleware to parse JSON request bodies
 app.use(express.json());
 // Rate limiting middleware
@@ -28,8 +32,13 @@ app.use(rateLimiter);
 // })
 // Use NotesRoutes for all /api/notesapp endpoints
 app.use("/api/notesapp", NotesRoutes);
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../FrontEnd/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../FrontEnd", "dist", "index.html"));
+  });
+}
 
-const PORT = process.env.PORT || 5001; // Set the port to the value in .env or default to 5001
 
 // Start the server on the specified port
 // Connect to MongoDB
--- FrontEnd/src/lib/axios.js
+++ FrontEnd/src/lib/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+// in production, there is no localhost so we need to have to make this dynamic
+// in development, we are using localhost:5001/api
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
-  baseURL: "http://localhost:5001/api"
+  baseURL: BASE_URL,
 })
 export default api;
